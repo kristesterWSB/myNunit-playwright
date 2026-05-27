a7a4ed913b0b70dffe70adfd31d12362ef928150
KrisQAaccount <kgarus92@gmail.com>
2026-05-27 23:41:26 +0200

    test(E2E) : added test ShouldCompleteOrderProcess

--- Tests/E2ETests.cs
+++ Tests/E2ETests.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace myNUnit.Tests
+﻿namespace myNUnit.Tests
 {
     public class E2ETests : TestBase
     {
@@ -12,6 +10,25 @@ namespace myNUnit.Tests
             await inventoryPage.ClickAddToCartBikeLightAsync("Sauce Labs Bike Light");
             var cartPage = await inventoryPage.GoToCartAsync();
             await cartPage.ValidteCartItem("Sauce Labs Bike Light");
+        }
+
+        [Test]
+        public async Task ShouldCompleteOrderProcess()
+        {
+            var inventoryPage = await LoginViaCookieAsync();
+            await inventoryPage.ClickAddToCartBikeLightAsync("Sauce Labs Bike Light");
+
+            var cartPage = await inventoryPage.GoToCartAsync();
+            await cartPage.ValidteCartItem("Sauce Labs Bike Light");
+
+            var checkoutInfoPage = await cartPage.ClickCheckoutButton();
+            await checkoutInfoPage.FillCheckoutFormAsync("John", "Doe", "12345");
+
+            var checkoutOverviewPage = await checkoutInfoPage.ClickContinueButtonAsync();
+
+            var checkoutCompletePage = await checkoutOverviewPage.ClickFinishButtonAsync();
+            await checkoutCompletePage.ValidateCheckoutCompletedOrderAsync("Thank you for your order!");
+
         }
     }
 }
