89326fca45e2122db7c36573d7893e78d74193ac
KrisQAaccount <kgarus92@gmail.com>
2026-05-28 00:06:04 +0200

    test(E2E) : added test which using advanced FluentApi

---
+++ Tests/E2EAdvancedFluentTests.cs
@@ -0,0 +1,23 @@
+﻿using myNUnit.Extensions;
+
+
+namespace myNUnit.Tests
+{
+    public class E2EAdvancedFluentTests : TestBase
+    {
+        [Test]
+        public async Task ShouldCompleteOrderProcessAdvancedFluent()
+        {
+            await LoginViaCookieAsync()
+                .Then(inventory => inventory.ClickAddToCartBikeLightAsync("Sauce Labs Bike Light"))
+                .Then(inventory => inventory.GoToCartAsync())
+                .Then(cart => cart.ValidteCartItem("Sauce Labs Bike Light"))
+                .Then(cart => cart.ClickCheckoutButton())
+                .Then(checkoutInfo => checkoutInfo.FillCheckoutFormAsync("John", "Doe", "12345"))
+                .Then(checkoutInfo => checkoutInfo.ClickContinueButtonAsync())
+                .Then(checkoutOverview => checkoutOverview.ClickFinishButtonAsync())
+                .Then(checkoutComplete => checkoutComplete.ValidateCheckoutCompletedOrderAsync("Thank you for your order!"));
+
+        }
+    }
+}
