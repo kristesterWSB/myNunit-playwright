217d3f92ab2e14682e9852b48d404619678cd0c9
KrisQAaccount <kgarus92@gmail.com>
2026-05-28 00:05:31 +0200

    test(FluentApi) : added FluentAPI chain

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace myNUnit.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async Task<TResult> Then<TSource, TResult>(
+            this Task<TSource> currentTask, 
+            Func<TSource, Task<TResult>> nextAction)
+        {
+            var result = await currentTask;
+
+            return await nextAction(result);
+        }
+
+        public static async Task<TSource> Then<TSource>(
+            this Task<TSource> currentTask, 
+            Func<TSource, Task> nextAction)
+        {
+            var result = await currentTask;
+            await nextAction(result);
+            return result;
+        }
+    }
+}
